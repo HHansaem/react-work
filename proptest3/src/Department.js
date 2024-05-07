2304685fe84df3c8cb42aaa2fd01770d6fa6a097
HHansaem <hhs01071331@gmail.com>
2024-05-07 12:04:04 +0900

    proptest3 수정

--- proptest3/src/Department.js
+++ proptest3/src/Department.js
@@ -11,12 +11,17 @@
 //     }
 // }
 
-const Department = ({depts}) => {
+// export default Department;
+
+//하나만 export하면 App.js에서 import {Department} 로 적어줘야 함
+export const Department = ({depts}) => {
     return(
-        depts.map(dept=> (
-            <option key={dept.id} value={dept.id}>{dept.name}</option>
-        ))
+        <select name="department">
+            {
+                depts.map(dept=> (
+                    <option key={dept.id} value={dept.id}>{dept.name}</option>
+                ))
+            }
+        </select>
     )
 }
-
-export default Department;

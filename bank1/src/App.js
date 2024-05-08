1c4fec925817c9f5927395de26d6aa65e1c41c1c
HHansaem <hhs01071331@gmail.com>
2024-05-08 16:16:50 +0900

    bank header수정(bootstrap)

--- bank1/src/App.js
+++ bank1/src/App.js
@@ -1,4 +1,5 @@
 // import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import { MakeAccount } from './MakeAccount';
 import AccountInfo from './AccountInfo';
 import Deposit from './Deposit';
@@ -16,13 +17,13 @@ function App() {  //<Routes> 밖: 고정적인 거, <Routes>안의 <Route>: path
       <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path="/" element={<MakeAccount />}/>
+        <Route exact path="/makeAccount" element={<MakeAccount />}/>
         <Route exact path="/accountInfo" element={<AccountInfo />}/>
         <Route exact path="/deposit" element={<Deposit />}/>
         <Route exact path="/withdraw" element={<Withdraw />}/>
         <Route exact path="/allAccountInfo" element={<AllAccountInfo />}/>
         <Route exact path="/join" element={<Join />}/>
-        <Route exact path="/login" element={<Login />}/>
+        <Route exact path="/" element={<Login />}/>
       </Routes>
       </BrowserRouter>
     </div>

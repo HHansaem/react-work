712aecd2463c09d013a6e5655d73a54b97b9fa5b
HHansaem <hhs01071331@gmail.com>
2024-05-08 17:59:15 +0900

    bank 계좌개설 reactstrap 적용

--- bank1/src/MakeAccount.js
+++ bank1/src/MakeAccount.js
@@ -1,62 +1,66 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import axios from 'axios';
+import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export const MakeAccount = () => {
-    const [acc, setAcc] = useState({id:'', name:'', balance:'', type:'normal', grade:''});
+    const [acc, setAcc] = useState({ id: '', name: '', balance: '', type: 'normal', grade: '' });
     const changeValue = (e) => {
-        setAcc({...acc, [e.target.name]:e.target.value})
+        setAcc({ ...acc, [e.target.name]: e.target.value })
     }
     const submit = (e) => {
         e.preventDefault();
         axios.post('http://localhost:8090/makeAccount', acc)
-            .then(res=>{
+            .then(res => {
                 alert(res.data);
             })
     }
 
-    return(
+    return (
         <>
             <div className='route'>
                 <h3>계좌개설</h3>
-                <table border="1">
-                    <tbody>
-                        <tr>
-                            <th>계좌번호</th>
-                            <td><input type="text" name="id" onChange={changeValue}/></td>
-                        </tr>
-                        <tr>
-                            <th>이름</th>
-                            <td><input type="text" name="name" onChange={changeValue}/></td>
-                        </tr>
-                        <tr>
-                            <th>입금액</th>
-                            <td><input type="text" name="balance" onChange={changeValue}/></td>
-                        </tr>
-                        <tr>
-                            <th>종류</th>
-                            <td>
-                                <input type="radio" name="type" value="normal" onChange={changeValue} checked={acc.type=='normal'}/>일반
-                                <input type="radio" name="type" value="special" onChange={changeValue}/>특수
-                            </td>
-                        </tr>
-                        <tr>
-                            <th>등급</th>
-                            <td>
-                                <select name="grade" onChange={changeValue} disabled={acc.type=='normal'}>
-                                    <option value="">선택하세요</option>
-                                    <option value="vip">VIP</option>
-                                    <option value="gold">Gold</option>
-                                    <option value="silver">Silver</option>
-                                    <option value="normal">Normal</option>
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td></td>
-                            <td><input type="submit" value="개설" onClick={submit}/></td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Form>
+                    <FormGroup row>
+                        <Label for="id" sm={3}>계좌번호</Label>
+                        <Col sm={9}>
+                            <Input type="text" name="id" id="id" onChange={changeValue} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="name" sm={3}>이름</Label>
+                        <Col sm={9}>
+                            <Input type="text" name="name" id="name" onChange={changeValue} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="balance" sm={3}>입금액</Label>
+                        <Col sm={9}>
+                            <Input type="text" name="balance" id="balance" onChange={changeValue} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label check for="type" sm={3}>종류</Label>
+                        <Col sm={9}>
+                            <Input type="radio" name="type" id="type" value="normal" onChange={changeValue} checked={acc.type == 'normal'} />
+                            일반
+                            <Input type="radio" name="type" id="type" value="special" onChange={changeValue} />
+                            특수
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label for="grade" sm={3}>등급</Label>
+                        <Col sm={9}>
+                            <Input type="select" name="grade" onChange={changeValue}>
+                                <option value="">선택하세요</option>
+                                <option value="vip">VIP</option>
+                                <option value="gold">Gold</option>
+                                <option value="silver">Silver</option>
+                                <option value="normal">Normal</option>
+                            </Input>
+                        </Col>
+                    </FormGroup>
+                    <Button onClick={submit}>개설</Button>
+                </Form>
             </div>
         </>
     )

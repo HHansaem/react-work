eac753d43d84c2ad606e226f88a8ff9561064eb7
HHansaem <hhs01071331@gmail.com>
2024-05-13 10:28:00 +0900

    reducer test 프로젝트 생성

---
+++ reducerex1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {useState, uesReducer} from 'react';
+
+// reducer : state를 업데이트하는 역할 즉, 함수이다.
+// action : 업데이트하고자 하는 내용
+// dispatch : state 업데이트를 위한 요구
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;

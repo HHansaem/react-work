a9e08ce2becc1e57822bee2c2f5dd5aaad65da4e
HHansaem <hhs01071331@gmail.com>
2024-05-17 15:10:59 +0900

    font에서 데이터 받기

--- reactstart2/src/User.js
+++ reactstart2/src/User.js
@@ -1,7 +1,41 @@
+import {useState} from 'react';
+
 const User = () => {
+    const [user, setUser] = useState({id:'', tel:'', email:'', address:''});
+
+    const changeValue = (e) => {
+        setUser({...user, [e.target.name]:e.target.value});
+    }
+
+    const submit = (e) => {
+        console.log(user);
+    }
+
     return(
         <div>
-            
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <td><label for="name">이름</label></td>
+                        <td><input type="text" id="name" name="name" onChange={changeValue}/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="tel">전화번호</label></td>
+                        <td><input type="text" id="tel" name="tel" onChange={changeValue}/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="email">이메일</label></td>
+                        <td><input type="text" id="email" name="email" onChange={changeValue}/></td>
+                    </tr>
+                    <tr>
+                        <td><label for="address">주소</label></td>
+                        <td><input type="text" id="address" name="address" onChange={changeValue}/></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2"><button onClick={submit}>전송</button></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }

f066833251bc1bfb20b26a26f262dac2b3c640f6
HHansaem <hhs01071331@gmail.com>
2024-05-17 17:17:32 +0900

    router를 사용해 경로에 따라 화면 다르게 주기

--- reactstart2/src/App.js
+++ reactstart2/src/App.js
@@ -3,15 +3,17 @@ import User from './User';
 import UserInfo from './UserInfo';
 import UserInfoForm from './UserInfoForm';
 import UserModify from './UserModify';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      {/* <User/> */}
-      {/* <UserInfo/> */}
-      {/* <UserModify/> */}
-      <UserInfoForm/>
-    </div>
+      //Routes 안에 있는 거 : 전체 화면 갱신
+      <Routes>
+        <Route exact path="/" element={<User />} />
+        <Route exact path="/userInfo" element={<UserInfo />} />
+        <Route exact path="/userModify" element={<UserModify />} />
+        <Route exact path="/userInfoForm" element={<UserInfoForm />} />
+      </Routes>
   );
 }
 
--- reactstart2/src/UserInfoForm.js
+++ reactstart2/src/UserInfoForm.js
@@ -2,31 +2,58 @@ import { useState } from "react";
 import axios from "axios";
 
 const UserInfoForm = () => {
-    const [name, setName] = useState({name:''});
-    const [user, setUser] = useState({name:'', tel:'', email:'', address:''});
+    const [name, setName] = useState({ name: '' });
+    const [user, setUser] = useState({ name: '', tel: '', email: '', address: '' });
+    const [isUser, setIsUser] = useState(false);
 
     const submit = (e) => {
         axios.get(`http://localhost:8090/getUser1/${name}`)
-            .then(res=> {
+            .then(res => {
                 console.log(res.data);
+                setUser({...res.data});
+                setIsUser(true);
             })
-            .catch(err=> {
+            .catch(err => {
                 console.log(err);
             })
     }
 
-    return(
-        <table border="1">
-            <tbody>
-                <tr>
-                    <td>이름</td>
-                    <td>
-                        <input type="text" onChange={(e)=>setName(e.target.value)}/>
-                        <button onClick={submit}>조회</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+    return (
+        <div>
+            <table border="1">
+                <tbody>
+                    <tr>
+                        <td>이름</td>
+                        <td>
+                            <input type="text" onChange={(e) => setName(e.target.value)} />
+                            <button onClick={submit}>조회</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            {isUser &&
+                <table border="1">
+                    <tbody>
+                        <tr>
+                            <td>이름</td>
+                            <td>{user.name}</td>
+                        </tr>
+                        <tr>
+                            <td>전화번호</td>
+                            <td>{user.tel}</td>
+                        </tr>
+                        <tr>
+                            <td>이메일</td>
+                            <td>{user.email}</td>
+                        </tr>
+                        <tr>
+                            <td>주소</td>
+                            <td>{user.address}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            }
+        </div>
     )
 }
 
